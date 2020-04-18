04baa72858248844aac6387ac944829ae3edda0f
haryanapnx <hary.vai@gmail.com>
2020-04-19 01:40:46 +0700

    update url api

--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -1,5 +1,5 @@
 const production = {
-  URL: "https://corona.lmao.ninja",
+  URL: "https://corona.lmao.ninja/v2",
   URL_ID:
     "https://cors-anywhere.herokuapp.com/https://indonesia-covid-19.mathdro.id/api"
 };
