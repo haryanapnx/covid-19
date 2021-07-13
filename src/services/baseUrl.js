c55d9b91b33de5e71e8c1d099d22bc3e41bfa64d
Haryana <hary.vai@gmail.com>
2021-07-14 00:00:18 +0700

    Update baseUrl.js

--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -1,7 +1,6 @@
 const production = {
   URL: "https://corona.lmao.ninja/v2",
-  URL_ID:
-    "https://cors-anywhere.herokuapp.com/https://indonesia-covid-19.mathdro.id/api"
+  URL_ID: "https://indonesia-covid-19.mathdro.id/api"
 };
 
 export const { URL, URL_ID } = production;
